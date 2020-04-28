7013697d0bb1d703819676d08fbc35f678a1580a
Pinkie Pie <yarnovax@gmail.com>
2020-04-28 11:26:13 -0400

    Add integrity check validator

---
+++ fixes/integrity-check.js
@@ -0,0 +1,16 @@
+// Validates that our packet integrity checks are still working
+
+module.exports = function IntegrityCheck(mod) {
+	let good = 0
+	let hook = mod.hook('*', 'raw', { order: -Infinity, filter: { incoming: false } }, (code, data) => {
+		if(!mod.dispatch.protocol.getIntegrity(code)) return
+
+		if(!mod.dispatch.packetIntegrity.validate(data)) {
+			mod.log.fatal('Packet integrity check failed! Terminating connection.')
+			mod.dispatch.connection.close()
+			return
+		}
+
+		if(++good === 20) mod.unhook(hook) // We're done. No more overhead
+	})
+}
